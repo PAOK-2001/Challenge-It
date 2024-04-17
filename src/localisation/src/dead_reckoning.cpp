f17863109087477ad4e61d4c62a7bdff421a7d2d
Pablo Ortega <2001.paok@gmail.com>
2024-04-17 17:52:22 -0600

    Add localisation node and transform node

--- src/localisation/src/dead_reckoning.cpp
+++ src/localisation/src/dead_reckoning.cpp
@@ -1,14 +1,72 @@
+#include <cmath>
+#include <chrono>
 #include <ros/ros.h>
 #include <ros/console.h>
-#include <geometry_msgs/Twist.h>
 #include <std_msgs/Float32.h>
-#include <chrono>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
 
 using namespace std;
 int nodeRate = 100;
 
+float right_speed = 0; 
+float left_speed = 0;
+
+
+void receive_right_speed(const std_msgs::Float32 &received_speed){
+    right_speed = received_speed.data;
+}
+
+void receive_left_speed(const std_msgs::Float32 &received_speed){
+    left_speed = received_speed.data;
+}
+
 int main(int argc, char *argv[]){
-    ros::init(argc, argv, "dead_reckoning_localisation");
+    ros::init(argc, argv, "dead_reckoning");
     ros::NodeHandle handler;
     ros::Rate rate(nodeRate);
+
+    ros::Publisher odom_pub = handler.advertise<nav_msgs::Odometry>("/odom", 10);
+    ros::Subscriber wl = handler.subscribe("/wr", 10, receive_right_speed);
+    ros::Subscriber wr = handler.subscribe("/wl", 10, receive_left_speed);
+
+     float _WHEELBASE, _WHEELRADIUS;
+    // Get robot parameters
+    ros::param::get("/wheel_base", _WHEELBASE);
+    ros::param::get("/wheel_radius", _WHEELRADIUS);
+
+    nav_msgs::Odometry odom;
+    float velocity, angular_vel;
+
+    float robot_x = 0;
+    float robot_y = 0;
+    float robot_theta = 0;
+    float dt = 0;
+
+     while(ros::ok){
+        chrono::steady_clock::time_point t = chrono::steady_clock::now();
+
+        velocity = _WHEELRADIUS*(right_speed + left_speed)/2;
+        angular_vel = _WHEELRADIUS*(right_speed - left_speed)/_WHEELBASE;
+
+        robot_x = robot_x + ((velocity * cos(robot_theta))*dt);
+        robot_y = robot_y + ((velocity * sin(robot_theta))*dt);
+        robot_theta =robot_theta + (angular_vel * dt);
+
+        odom.pose.pose.position.x = robot_x;
+        odom.pose.pose.position.y = robot_y;
+        odom.pose.pose.orientation.z = robot_theta;
+
+        odom.twist.twist.linear.x = velocity;
+        odom.twist.twist.angular.z = angular_vel;
+
+
+        odom_pub.publish(odom);
+
+        ros::spinOnce();
+        rate.sleep();     
+        dt = (chrono::duration_cast<chrono::microseconds> (chrono::steady_clock::now() - t).count())/1000000.0; // Time difference in seconds
+     
+     }
+    
 }
---
+++ src/localisation/src/joint_state_publisher.cpp
@@ -0,0 +1,39 @@
+#include <ros/ros.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <nav_msgs/Odometry.h>
+
+int nodeRate = 100;
+
+void receive_odom(const nav_msgs::Odometry &received_odom){
+    static tf2_ros::TransformBroadcaster br;
+    geometry_msgs::TransformStamped transformStamped;
+
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "base_link";
+    transformStamped.child_frame_id = "chassis";
+    transformStamped.transform.translation.x = received_odom.pose.pose.position.x;
+    transformStamped.transform.translation.y = received_odom.pose.pose.position.y;
+    transformStamped.transform.translation.z = 0.0;
+    
+    tf2::Quaternion q;
+    q.setRPY(0, 0, received_odom.pose.pose.orientation.z);
+    transformStamped.transform.rotation.x = q.x();
+    transformStamped.transform.rotation.y = q.y();
+    transformStamped.transform.rotation.z = q.z();
+    transformStamped.transform.rotation.w = q.w();
+
+    br.sendTransform(transformStamped);
+}
+
+int main(int argc, char ** argv){
+    ros::init(argc, argv, "Joint_states");  
+    ros::NodeHandle handler;
+    ros::Rate rate(nodeRate);
+
+    ros::Subscriber sub = handler.subscribe("/odom", 10, receive_odom);
+    ros::spin();
+    return 0;
+
+}
